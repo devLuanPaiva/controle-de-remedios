dbb13bfa05eea0bef89f894bcad8608658082ac8
Luan Alves <devluanpaiva@gmail.com>
2026-06-27 13:25:59 -0300

    feat: creating api application

---
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/ControleDeRemediosApplication.java
@@ -0,0 +1,13 @@
+package com.devluanpaiva.controle_de_remedios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeRemediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeRemediosApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/devluanpaiva/controle_de_remedios/ControleDeRemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluanpaiva.controle_de_remedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeRemediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
