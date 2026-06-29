81723b37dd35d12fa9e4ae5e3485bc6ab233a696
Luan Alves <devluanpaiva@gmail.com>
2026-06-29 08:02:12 -0300

    feat(api): creating UserRepository interface

---
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.devluanpaiva.controle_de_remedios.modules.users.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devluanpaiva.controle_de_remedios.modules.users.entity.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
