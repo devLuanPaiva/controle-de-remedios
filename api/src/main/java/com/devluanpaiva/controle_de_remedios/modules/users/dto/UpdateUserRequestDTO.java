6e27987f99300abbb3063182be9f0aafe93e291d
Luan Alves <devluanpaiva@gmail.com>
2026-06-29 11:27:49 -0300

    refactor(api): removing role field from update DTO

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/dto/UpdateUserRequestDTO.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/dto/UpdateUserRequestDTO.java
@@ -1,13 +1,10 @@
 package com.devluanpaiva.controle_de_remedios.modules.users.dto;
 
-import com.devluanpaiva.controle_de_remedios.modules.users.enums.UserRole;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UpdateUserRequestDTO(
-                @NotBlank String name,
-                @NotBlank @Size(min = 11, max = 11) String cpf,
-                String imageUrl,
-                @NotBlank UserRole role) {
+        @NotBlank String name,
+        @NotBlank @Size(min = 11, max = 11) String cpf,
+        String imageUrl) {
 }
