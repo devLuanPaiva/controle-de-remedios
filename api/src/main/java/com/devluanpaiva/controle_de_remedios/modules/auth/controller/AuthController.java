ea788482eaef1aa351fc7991e6a061338fc68a88
Luan Alves <devluanpaiva@gmail.com>
2026-06-30 09:55:51 -0300

    feat(api): creating AuthController class

---
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.devluanpaiva.controle_de_remedios.modules.auth.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import com.devluanpaiva.controle_de_remedios.modules.auth.dto.AuthResponseDTO;
+import com.devluanpaiva.controle_de_remedios.modules.auth.dto.LoginRequestDTO;
+import com.devluanpaiva.controle_de_remedios.modules.auth.dto.RefreshTokenRequestDTO;
+import com.devluanpaiva.controle_de_remedios.modules.auth.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public AuthResponseDTO login(
+            @RequestBody @Valid LoginRequestDTO dto) {
+        return authService.login(dto);
+    }
+
+    @PostMapping("/refresh")
+    public AuthResponseDTO refresh(
+            @RequestBody RefreshTokenRequestDTO dto) {
+
+        return authService.refresh(dto);
+    }
+}
