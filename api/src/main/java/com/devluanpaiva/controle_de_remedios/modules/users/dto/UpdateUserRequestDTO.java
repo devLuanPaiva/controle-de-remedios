fa16f8ab7f8e504424591aabde8e40c02b695acd
Luan Alves <devluanpaiva@gmail.com>
2026-06-30 13:16:29 -0300

    fix(api): removing the requirement for fields when editing a user

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/dto/UpdateUserRequestDTO.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/dto/UpdateUserRequestDTO.java
@@ -1,10 +1,7 @@
 package com.devluanpaiva.controle_de_remedios.modules.users.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 public record UpdateUserRequestDTO(
-        @NotBlank String name,
-        @NotBlank @Size(min = 11, max = 11) String cpf,
-        String imageUrl) {
+                String name,
+                String cpf,
+                String imageUrl) {
 }
