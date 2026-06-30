37e143c19d40506b3cc9acd8d2d0c498739387cf
Luan Alves <devluanpaiva@gmail.com>
2026-06-30 16:18:30 -0300

    refactor(desktop): clearning app

--- desktop/src/app/app.component.ts
+++ desktop/src/app/app.component.ts
@@ -9,14 +9,5 @@ import { invoke } from "@tauri-apps/api/core";
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
+ 
 }
