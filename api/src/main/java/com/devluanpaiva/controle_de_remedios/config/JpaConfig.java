f6653c32b3870cc37da898e949ec34b16c3f1c80
Luan Alves <devluanpaiva@gmail.com>
2026-06-30 09:53:26 -0300

    feat(api): creating JpaConfig class

---
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.devluanpaiva.controle_de_remedios.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
