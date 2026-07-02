66f4232eceec4c44cffe94149863e81692788103
Luan Alves <devluanpaiva@gmail.com>
2026-07-02 11:20:23 -0300

    feat(desktop): creating auth-card component

---
+++ desktop/src/app/features/auth/components/auth-card/auth-card.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+    selector: 'app-auth-card',
+    imports: [NgOptimizedImage],
+    templateUrl: './auth-card.html',
+    styleUrl: './auth-card.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AuthCard {
+    readonly logoSrc = input('/assets/logo.png');
+    readonly title = input.required<string>();
+    readonly subtitle = input('');
+}
