f21f3f76e2320466c95d124a54c9f634eb3400e6
Luan Alves <devluanpaiva@gmail.com>
2026-07-03 16:22:09 -0300

    chore(mobile): installing expo google fonts

--- mobile/src/theme/typography.ts
+++ mobile/src/theme/typography.ts
@@ -1,9 +1,11 @@
 export const Typography = {
-    heading: "BebasNeue_400Regular",
-    body: "Montserrat_400Regular",
-    bodyMedium: "Montserrat_500Medium",
-    bodySemiBold: "Montserrat_600SemiBold",
-    bodyBold: "Montserrat_700Bold",
+    fonts: {
+        heading: "BebasNeue_400Regular",
+        body: "Montserrat_400Regular",
+        bodyMedium: "Montserrat_500Medium",
+        bodySemiBold: "Montserrat_600SemiBold",
+        bodyBold: "Montserrat_700Bold",
+    },
 
     sizes: {
         xs: 11,
@@ -14,11 +16,11 @@ export const Typography = {
         xl: 20,
         xxl: 24,
     },
-    
+
     weights: {
-        normal: '400' as const,
-        medium: '500' as const,
-        semibold: '600' as const,
-        bold: '700' as const,
+        normal: "400" as const,
+        medium: "500" as const,
+        semibold: "600" as const,
+        bold: "700" as const,
     },
 } as const;
