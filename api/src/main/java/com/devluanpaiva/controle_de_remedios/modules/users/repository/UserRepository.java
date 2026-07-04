ae4206a1f2add4d88ab204ff6c2b58852701078f
Luan Alves <devluanpaiva@gmail.com>
2026-07-04 14:11:51 -0300

    refactor(api): Defining a rule to prevent a user from managing other users based on their system role.

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/repository/UserRepository.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.devluanpaiva.controle_de_remedios.modules.users.repository;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devluanpaiva.controle_de_remedios.modules.users.entity.User;
+import com.devluanpaiva.controle_de_remedios.modules.users.enums.UserRole;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
@@ -15,4 +19,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByCpf(String cpf);
 
     Optional<User> findById(UUID id);
+
+    Page<User> findByRoleIn(Collection<UserRole> roles, Pageable pageable);
 }
