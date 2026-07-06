1751dd8092d1adb9f7dbae9bd7f410a0c5358755
Luan Alves <devluanpaiva@gmail.com>
2026-07-06 09:25:36 -0300

    refactor(desktop): removing exit button from home component

--- desktop/src/app/features/auth/pages/home/home.ts
+++ desktop/src/app/features/auth/pages/home/home.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import * as AuthActions from '../../store/auth.actions';
 import { AuthSessionService } from '../../services/auth-session.service';
 
 @Component({
@@ -11,12 +9,7 @@ import { AuthSessionService } from '../../services/auth-session.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
-    private readonly store = inject(Store);
     private readonly session = inject(AuthSessionService);
 
     readonly user = this.session.user;
-
-    onLogout(): void {
-        this.store.dispatch(AuthActions.logout());
-    }
 }
