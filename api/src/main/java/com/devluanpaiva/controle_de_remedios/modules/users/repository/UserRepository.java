86b9a51399856af7fec18e7f64e0211f55420b13
Luan Alves <devluanpaiva@gmail.com>
2026-07-08 08:29:19 -0300

    refactor(api): removing findByRoleIn query from user repository

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/repository/UserRepository.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/users/repository/UserRepository.java
@@ -1,17 +1,16 @@
 package com.devluanpaiva.controle_de_remedios.modules.users.repository;
 
-import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.devluanpaiva.controle_de_remedios.modules.users.entity.User;
-import com.devluanpaiva.controle_de_remedios.modules.users.enums.UserRole;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
@@ -19,8 +18,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByCpf(String cpf);
 
     Optional<User> findById(UUID id);
-
-    Page<User> findByRoleIn(Collection<UserRole> roles, Pageable pageable);
 
     Page<User> findByCompanies_Id(UUID companyId, Pageable pageable);
 }
