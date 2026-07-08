2df6b0ff39d7dee90420e8350b8d9f1e8934d19c
Luan Alves <devluanpaiva@gmail.com>
2026-07-08 14:10:28 -0300

    feat(api): validating create and update fields and creating CNPJ validator class

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/company/dto/CreateCompanyRequestDTO.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/company/dto/CreateCompanyRequestDTO.java
@@ -1,10 +1,12 @@
 package com.devluanpaiva.controle_de_remedios.modules.company.dto;
 
+import com.devluanpaiva.controle_de_remedios.shared.validation.Cnpj;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateCompanyRequestDTO(
         @NotBlank @Size(max = 120) String name,
-        @NotBlank @Size(min = 14, max = 14) String cnpj,
-        String imageUrl) {
+        @NotBlank @Cnpj String cnpj,
+        @Size(max = 255) String imageUrl) {
 }
--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/company/dto/UpdateCompanyRequestDTO.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/company/dto/UpdateCompanyRequestDTO.java
@@ -1,7 +1,9 @@
 package com.devluanpaiva.controle_de_remedios.modules.company.dto;
 
+import jakarta.validation.constraints.Size;
+
 public record UpdateCompanyRequestDTO(
-        String name,
-        String imageUrl,
+        @Size(min = 1, max = 120) String name,
+        @Size(max = 255) String imageUrl,
         Boolean active) {
 }
