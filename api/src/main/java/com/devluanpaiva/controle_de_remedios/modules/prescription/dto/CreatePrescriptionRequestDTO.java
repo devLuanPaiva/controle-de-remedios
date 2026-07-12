9ca896d47740f7eb13ce73c1155fbab889f384dd
Luan Alves <devluanpaiva@gmail.com>
2026-07-12 13:10:00 -0300

    refactor(api): adapting all the code to support multiple images in the prescription entity

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/prescription/dto/CreatePrescriptionRequestDTO.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/prescription/dto/CreatePrescriptionRequestDTO.java
@@ -1,6 +1,7 @@
 package com.devluanpaiva.controle_de_remedios.modules.prescription.dto;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.validation.constraints.NotNull;
@@ -8,7 +9,7 @@ import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
 public record CreatePrescriptionRequestDTO(
-        @Size(max = 255) String imageUrl,
+        List<@Size(max = 255) String> imageUrls,
         @NotNull @PastOrPresent LocalDate issueDate,
         @NotNull UUID patientId) {
 }
