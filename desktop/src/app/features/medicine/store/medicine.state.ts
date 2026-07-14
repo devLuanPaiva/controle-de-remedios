b55485bf0a38f2656fc9576a0886685db5125029
Luan Alves <devluanpaiva@gmail.com>
2026-07-14 07:18:53 -0300

    feat(desktop): implementing medicine store: - actions - reducer - state - selectors - effects

---
+++ desktop/src/app/features/medicine/store/medicine.state.ts
@@ -0,0 +1,15 @@
+import { IMedicine } from '../models/medicine.model';
+
+export interface MedicineState {
+    items: IMedicine[];
+
+    loading: boolean;
+    error: string | null;
+    mutating: boolean;
+
+    count: number;
+    currentPage: number;
+    totalPages: number;
+    next: string | null;
+    previous: string | null;
+}
