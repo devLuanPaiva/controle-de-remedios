13fb8daa569cea368a27be2236c1f8807fdebff4
Luan Alves <devluanpaiva@gmail.com>
2026-07-15 07:30:46 -0300

    feat(api): creating DeliveryMapper class

---
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/delivery/mapper/DeliveryMapper.java
@@ -0,0 +1,22 @@
+package com.devluanpaiva.controle_de_remedios.modules.delivery.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.devluanpaiva.controle_de_remedios.modules.delivery.dto.DeliveryResponseDTO;
+import com.devluanpaiva.controle_de_remedios.modules.delivery.entity.Delivery;
+
+@Component
+public class DeliveryMapper {
+    public DeliveryResponseDTO toResponseDTO(Delivery delivery) {
+        return new DeliveryResponseDTO(
+                delivery.getId(),
+                delivery.getCompany().getId(),
+                delivery.getPatient().getId(),
+                delivery.getPrescriptionItem().getId(),
+                delivery.getDeliveryDate(),
+                delivery.getNextAvailableDate(),
+                delivery.getDeliveryQuantity(),
+                delivery.getCreatedAt(),
+                delivery.getUpdatedAt());
+    }
+}
