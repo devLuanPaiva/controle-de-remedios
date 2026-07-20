ce993eb9a821f71b051876a407496321e2448a1f
Luan Alves <devluanpaiva@gmail.com>
2026-07-20 11:23:34 -0300

    feat(api): creating auth dtos: - ForgotPasswordRrquestDTO - ResetPasswordRequestDTO

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/auth/dto/ResetPasswordRequestDTO.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/auth/dto/ResetPasswordRequestDTO.java
@@ -1,10 +1,10 @@
-package com.devluanpaiva.controle_de_remedios.modules.user.dto;
+package com.devluanpaiva.controle_de_remedios.modules.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record ChangePasswordRequestDTO(
-        @NotBlank String currentPassword,
+public record ResetPasswordRequestDTO(
+        @NotBlank String token,
         @NotBlank @Size(min = 6, max = 20) String newPassword,
         @NotBlank String confirmPassword) {
 }
