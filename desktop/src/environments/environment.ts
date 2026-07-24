d73e0643682c76e8eb78a6a356b0e1d071b661b3
Luan Alves <devluanpaiva@gmail.com>
2026-07-24 19:04:52 -0300

    refactor(updating environment

--- desktop/src/environments/environment.ts
+++ desktop/src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    api_url: 'http://localhost:8080',
-    googleOAuthClientIdDesktop: '216114981444-qe82bnfkpfuu41j575il5bncp2s0dj8p.apps.googleusercontent.com',
+    api_url: 'http://localhost:8080'
 };
